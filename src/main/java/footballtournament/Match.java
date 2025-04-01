ccc59cba843e76675523010c7ad19df57bb35b1b
Oluwarotimi Samuel Adegoju <oluwarotimisamuel.adegoju@griffithuni.edu.au>
2025-04-01 15:54:57 +0200

    Initial commit for JVA_FOOTBALL_TOURNAMENT

---
+++ src/main/java/footballtournament/Match.java
@@ -0,0 +1,11 @@
+package footballtournament;
+
+
+/**This is the Match Class*/
+public class Match {
+    public Team team1;
+    public Team team2;
+    public int score1;
+    public int score2;
+    public Team winner;
+}
---
+++ src/main/java/footballtournament/Tournament.java
@@ -0,0 +1,8 @@
+package footballtournament;
+
+/**This is the Tournament Class*/
+public class Tournament {
+    public String name;
+    public String teams;
+    public String matches;
+}
